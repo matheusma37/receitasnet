4e7b8fa67a39df96b95f21521455b62fbcfa0f23
matheusma37 <mtsma37@gmail.com>
2018-12-02 15:44:32 -0200

    Criando validadores nos modelos

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,13 @@
 class Recipe < ApplicationRecord
+	validates :name, :stuff, :duration, :prepare_mode, presence:true
+	validates_length_of :stuff, maximum: 200
+	validates_length_of :prepare_mode, maximum: 300
+	validates_numericality_of :calories, greather_than_or_equal_to:0, presence:true
+	validates_format_of :poster, with:/\w+\.(gif|jpeg|png)\z/i, message:"uses only GIF, JPEG or PNG formats."
+
+	KIND = %w{Carnes Aves Peixes Massas Saladas Doces}
+	validates_inclusion_of :kind, in: KIND
+
 	def light?
 		calories < 100
 	end
